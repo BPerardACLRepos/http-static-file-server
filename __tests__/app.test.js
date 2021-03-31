b3093684cae66201d65a2455a77f8a8fb4b765d2
Brandon Perard <brandon.perard@gmail>
2021-03-31 15:53:49 -0700

    add tests to app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,21 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const fs = require('fs').promises;
 
 describe('app routes', () => {
-    it()
+    it('response.text should be the path the user entered', async () => {
+        const path = '/yo';
+
+        const response = await request(app).get(path);
+
+        expect(response.text).toEqual(path);
+    });
+
+    it('response.text should be the contents of index.html when user path is /index.html', async () => {
+        const file = await fs.readFile(`public/index.html`, 'utf-8');
+
+        const response = await request(app).get('/index.html');
+
+        expect(response.text).toEqual(file);
+    });
 });
