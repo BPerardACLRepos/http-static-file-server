5d023fa0957180ca1c223a83e14591fa401016ad
Brandon Perard <brandon.perard@gmail>
2021-03-31 17:02:53 -0700

    update tests to resolve error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,6 @@ const app = require('../lib/app');
 const fs = require('fs').promises;
 
 describe('app routes', () => {
-    it('response.text should be the path the user entered', async () => {
-        const path = '/yo';
-
-        const response = await request(app).get(path);
-
-        expect(response.text).toEqual(path);
-    });
-
     it('response.text should be the contents of index.html when user path is /index.html', async () => {
         const file = await fs.readFile(`public/index.html`, 'utf-8');
 
@@ -19,8 +11,8 @@ describe('app routes', () => {
         expect(response.text).toEqual(file);
     });
 
-    it('response.text should be err.message', async () => {
-        const path = '/noFile.html';
+    it('response.text should be err.message text', async () => {
+        const path = '/noFile';
 
         const expected = `ENOENT: no such file or directory, open 'public${path}'`;
 
